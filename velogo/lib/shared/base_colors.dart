38ae132f12fd36e3fbc8591a0340ef99d359bc8d
Dmytro Solop <dmtsolop@gmail.com>
2024-11-26 21:07:11 +0200

    Added new colors

--- velogo/lib/shared/base_colors.dart
+++ velogo/lib/shared/base_colors.dart
@@ -1,11 +1,26 @@
-
 import 'package:flutter/material.dart';
 
-class AppColors {
+class BaseColors {
   static const Color background = Color(0xFF242527);
   static const Color primary = Color(0xFF32596A);
   static const Color white = Colors.white;
   static const Color grey = Color(0xFF94A3B8);
   static const Color textPrimary = Colors.white;
   static const Color textSecondary = Color(0xFF94A3B8);
+
+  // Нові кольори
+  static const Color iconSelected =
+      Color(0xFF4CAF50); // Зелений для активного стану
+  static const Color iconUnselected =
+      Color(0xFFBDBDBD); // Сірий для неактивного стану
+  static const Color inputBackground = Color(0xFF2E2E2E); // Фон для поля вводу
+  static const Color cardBackgroundDark =
+      Color(0xFF1C1C1C); // Темний фон для карток
+  static const Color bottomBarDark = Color(0xFF212121); // Фон для нижнього меню
+
+  // Кольори, які використовуються у MainScreen
+  static const Color backgroundDark = Color(0xFF1A1A1A);
+  static const Color headerDark = Color(0xFF333333);
+  static const Color iconLight = Colors.white; // Світла іконка
+  static const Color cardBackground = Color(0xFF424242); // Фон карток подій
 }
