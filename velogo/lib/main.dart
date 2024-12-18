538038076fc5f789eb2404e045925688e529170a
Dmytro Solop <dmtsolop@gmail.com>
2024-12-18 18:26:25 +0200

    Cleaning

--- velogo/lib/main.dart
+++ velogo/lib/main.dart
@@ -4,28 +4,24 @@ import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:path_provider/path_provider.dart';
 import 'bloc/settings/settings_cubit.dart';
 import 'bloc/theme/theme_cubit.dart';
-import 'bloc/navigation/navigation_cubit.dart';
 import 'shared/custom_bottom_navigation_bar.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   // Ініціалізація сховища для HydratedBloc
-  final storage = await HydratedStorage.build(
+  HydratedBloc.storage = await HydratedStorage.build(
     storageDirectory: await getApplicationDocumentsDirectory(),
   );
-  HydratedBlocOverrides.runZoned(
-    () => runApp(
-      MultiBlocProvider(
-        providers: [
-          BlocProvider(create: (_) => ThemeCubit()),
-          BlocProvider(create: (_) => SettingsCubit()),
-          BlocProvider(create: (_) => NavigationCubit()),
-        ],
-        child: const MyApp(),
-      ),
+
+  runApp(
+    MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (_) => ThemeCubit()),
+        BlocProvider(create: (_) => SettingsCubit()),
+      ],
+      child: const MyApp(),
     ),
-    storage: storage,
   );
 }
 
@@ -45,72 +41,9 @@ class MyApp extends StatelessWidget {
           theme: ThemeData.light(),
           darkTheme: ThemeData.dark(),
           themeMode: isDark ? ThemeMode.dark : ThemeMode.light,
-          home: BlocBuilder<NavigationCubit, NavigationState>(
-            builder: (context, state) {
-              if (state is NavigationStateBottomNav) {
-                return const CustomBottomNavigationBar();
-              } else if (state is NavigationStateAuth) {
-                // Наприклад, Start Screen у випадку логіну
-                return const StartScreen();
-              }
-              // Default fallback
-              return const CustomBottomNavigationBar();
-            },
-          ),
+          home: const CustomBottomNavigationBar(),
         );
       },
     );
   }
 }
-
-
-
-// import 'package:flutter/material.dart';
-// import 'package:flutter_bloc/flutter_bloc.dart';
-// import 'package:hydrated_bloc/hydrated_bloc.dart';
-// import 'package:path_provider/path_provider.dart';
-// import 'bloc/settings/settings_cubit.dart';
-// import 'bloc/theme/theme_cubit.dart';
-// import 'shared/custom_bottom_navigation_bar.dart';
-
-// void main() async {
-//   WidgetsFlutterBinding.ensureInitialized();
-
-//   // Ініціалізація сховища для HydratedBloc
-//   HydratedBloc.storage = await HydratedStorage.build(
-//     storageDirectory: await getApplicationDocumentsDirectory(),
-//   );
-
-//   runApp(
-//     MultiBlocProvider(
-//       providers: [
-//         BlocProvider(create: (_) => ThemeCubit()),
-//         BlocProvider(create: (_) => SettingsCubit()),
-//       ],
-//       child: const MyApp(),
-//     ),
-//   );
-// }
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({Key? key}) : super(key: key);
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return BlocBuilder<ThemeCubit, AppThemeMode>(
-//       builder: (context, themeMode) {
-//         final isDark = themeMode == AppThemeMode.dark ||
-//             (themeMode == AppThemeMode.system &&
-//                 MediaQuery.of(context).platformBrightness == Brightness.dark);
-
-//         return MaterialApp(
-//           title: 'Custom Navigation App',
-//           theme: ThemeData.light(),
-//           darkTheme: ThemeData.dark(),
-//           themeMode: isDark ? ThemeMode.dark : ThemeMode.light,
-//           home: const CustomBottomNavigationBar(),
-//         );
-//       },
-//     );
-//   }
-// }
