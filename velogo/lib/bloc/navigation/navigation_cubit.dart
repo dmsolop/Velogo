117884f13b58c24d1637b6b6c84ebacee3a8357b
Dmytro Solop <dmtsolop@gmail.com>
2024-12-23 21:35:28 +0200

    Implemented base navigation by Cubit

--- velogo/lib/bloc/navigation/navigation_cubit.dart
+++ velogo/lib/bloc/navigation/navigation_cubit.dart
@@ -2,7 +2,8 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'navigation_state.dart';
 
 class NavigationCubit extends Cubit<NavigationState> {
-  NavigationCubit() : super(const NavigationState());
+  NavigationCubit()
+      : super(const NavigationState(selectedTab: NavigationTab.home));
 
   void selectTab(NavigationTab tab) {
     emit(state.copyWith(selectedTab: tab));
--- velogo/lib/main.dart
+++ velogo/lib/main.dart
@@ -2,14 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:path_provider/path_provider.dart';
-import 'bloc/settings/settings_cubit.dart';
 import 'bloc/theme/theme_cubit.dart';
+import 'bloc/settings/settings_cubit.dart';
+import 'bloc/navigation/navigation_cubit.dart';
+import 'bloc/navigation/navigation_state.dart';
 import 'shared/custom_bottom_navigation_bar.dart';
+import 'screens/main_screen.dart';
+import 'screens/route_screen.dart';
+import 'screens/profile_screen.dart';
+import 'screens/settings_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  // Ініціалізація сховища для HydratedBloc
   HydratedBloc.storage = await HydratedStorage.build(
     storageDirectory: await getApplicationDocumentsDirectory(),
   );
@@ -18,7 +22,8 @@ void main() async {
     MultiBlocProvider(
       providers: [
         BlocProvider(create: (_) => ThemeCubit()),
-        BlocProvider(create: (_) => SettingsCubit()),
+        BlocProvider(create: (_) => SettingsCubit()), // Збережено
+        BlocProvider(create: (_) => NavigationCubit()),
       ],
       child: const MyApp(),
     ),
@@ -41,9 +46,86 @@ class MyApp extends StatelessWidget {
           theme: ThemeData.light(),
           darkTheme: ThemeData.dark(),
           themeMode: isDark ? ThemeMode.dark : ThemeMode.light,
-          home: const CustomBottomNavigationBar(),
+          home: BlocBuilder<NavigationCubit, NavigationState>(
+            builder: (context, state) {
+              Widget currentScreen;
+              switch (state.selectedTab) {
+                case NavigationTab.home:
+                  currentScreen = const MainScreen();
+                  break;
+                case NavigationTab.myRoutes:
+                  currentScreen = const RouteScreen();
+                  break;
+                case NavigationTab.profile:
+                  currentScreen = const ProfileScreen();
+                  break;
+                case NavigationTab.settings:
+                  currentScreen = const SettingsScreen();
+                  break;
+                default:
+                  currentScreen = const MainScreen();
+              }
+
+              return Scaffold(
+                body: currentScreen,
+                bottomNavigationBar: const CustomBottomNavigationBar(),
+              );
+            },
+          ),
         );
       },
     );
   }
 }
+
+
+
+// import 'package:flutter/material.dart';
+// import 'package:flutter_bloc/flutter_bloc.dart';
+// import 'package:hydrated_bloc/hydrated_bloc.dart';
+// import 'package:path_provider/path_provider.dart';
+// import 'bloc/settings/settings_cubit.dart';
+// import 'bloc/theme/theme_cubit.dart';
+// import 'shared/custom_bottom_navigation_bar.dart';
+
+// void main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+
+//   // Ініціалізація сховища для HydratedBloc
+//   HydratedBloc.storage = await HydratedStorage.build(
+//     storageDirectory: await getApplicationDocumentsDirectory(),
+//   );
+
+//   runApp(
+//     MultiBlocProvider(
+//       providers: [
+//         BlocProvider(create: (_) => ThemeCubit()),
+//         BlocProvider(create: (_) => SettingsCubit()),
+//       ],
+//       child: const MyApp(),
+//     ),
+//   );
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return BlocBuilder<ThemeCubit, AppThemeMode>(
+//       builder: (context, themeMode) {
+//         final isDark = themeMode == AppThemeMode.dark ||
+//             (themeMode == AppThemeMode.system &&
+//                 MediaQuery.of(context).platformBrightness == Brightness.dark);
+
+//         return MaterialApp(
+//           title: 'Custom Navigation App',
+//           theme: ThemeData.light(),
+//           darkTheme: ThemeData.dark(),
+//           themeMode: isDark ? ThemeMode.dark : ThemeMode.light,
+//           home: const CustomBottomNavigationBar(),
+//         );
+//       },
+//     );
+//   }
+// }
