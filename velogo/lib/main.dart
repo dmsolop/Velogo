6d74d9a14db46f4e332d01a7bbdda5b947c8f69a
Dmytro Solop <dmtsolop@gmail.com>
2025-01-13 20:39:07 +0200

    Fix: Resolve issue with HydratedStorageDirectory initialization
    - Updated HydratedBloc initialization to use `HydratedStorageDirectory` with a custom path.
    - Utilized `getApplicationDocumentsDirectory` from path_provider for directory path.
    - Ensured compatibility with updated `hydrated_bloc` and `flutter_bloc` versions.

--- velogo/lib/main.dart
+++ velogo/lib/main.dart
@@ -19,8 +19,10 @@ import 'package:cloud_functions/cloud_functions.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  final directory = await getApplicationDocumentsDirectory();
   HydratedBloc.storage = await HydratedStorage.build(
-    storageDirectory: await getApplicationDocumentsDirectory(),
+    storageDirectory: HydratedStorageDirectory(directory.path),
   );
 
   FirebaseFirestore.instance.settings = const Settings(
@@ -80,8 +82,6 @@ class MyApp extends StatelessWidget {
                 case NavigationTab.settings:
                   currentScreen = const SettingsScreen();
                   break;
-                default:
-                  currentScreen = const MainScreen();
               }
 
               return Scaffold(
@@ -95,53 +95,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-// import 'package:flutter/material.dart';
-// import 'package:flutter_bloc/flutter_bloc.dart';
-// import 'package:hydrated_bloc/hydrated_bloc.dart';
-// import 'package:path_provider/path_provider.dart';
-// import 'bloc/settings/settings_cubit.dart';
-// import 'bloc/theme/theme_cubit.dart';
-// import 'shared/custom_bottom_navigation_bar.dart';
-
-// void main() async {
-//   WidgetsFlutterBinding.ensureInitialized();
-
-//   // Ініціалізація сховища для HydratedBloc
-//   HydratedBloc.storage = await HydratedStorage.build(
-//     storageDirectory: await getApplicationDocumentsDirectory(),
-//   );
-
-//   runApp(
-//     MultiBlocProvider(
-//       providers: [
-//         BlocProvider(create: (_) => ThemeCubit()),
-//         BlocProvider(create: (_) => SettingsCubit()),
-//       ],
-//       child: const MyApp(),
-//     ),
-//   );
-// }
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({Key? key}) : super(key: key);
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return BlocBuilder<ThemeCubit, AppThemeMode>(
-//       builder: (context, themeMode) {
-//         final isDark = themeMode == AppThemeMode.dark ||
-//             (themeMode == AppThemeMode.system &&
-//                 MediaQuery.of(context).platformBrightness == Brightness.dark);
-
-//         return MaterialApp(
-//           title: 'Custom Navigation App',
-//           theme: ThemeData.light(),
-//           darkTheme: ThemeData.dark(),
-//           themeMode: isDark ? ThemeMode.dark : ThemeMode.light,
-//           home: const CustomBottomNavigationBar(),
-//         );
-//       },
-//     );
-//   }
-// }
